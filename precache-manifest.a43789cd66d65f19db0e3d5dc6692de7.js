115e706925be40385570539ae406a46e87f82b4e
SerGolub89 <k3210389gsi@gmail.com>
2019-08-14 12:47:29 +0300

    Updates

---
+++ precache-manifest.a43789cd66d65f19db0e3d5dc6692de7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e8d94164e8470cd3320478a94821a9",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "7b5ff41deae15e280e06",
+    "url": "/react_tabs/static/css/main.2c1d01ae.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "7b5ff41deae15e280e06",
+    "url": "/react_tabs/static/js/main.84bab5ce.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
